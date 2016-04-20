75ea56079bef023d40ef00a4b0f0995acf8d9aff
noahellis <noah_ellis@hotmail.com>
2016-04-20 10:45:57 -0500

    implemented interface

---
+++ SweepStakes/ICustomString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    interface ICustomString
+    {
+        
+        string ToString();
+        void Insert(string stringToInsert);
+        void Remove(int startIndex, int numCharsToRemove);
+        int length();
+
+
+    }
+}
